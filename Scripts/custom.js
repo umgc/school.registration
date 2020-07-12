43f95f2392c3b64b5f63302d787ce3d2124fb41c
Remrap <hardcoded@comcast.net>
2020-07-12 15:43:18 -0400

    Added placeholder text fiels to output folders Moved custom JS to custom file Made updates to master pages to reflect JS changes

---
+++ Scripts/custom.js
@@ -0,0 +1,33 @@
+// MASK PHONE NUMBERS
+// ==============
+
+$(document).ready(function () {
+    $('.phonenum').blur(function () {
+        var val = $(this).val();
+        var newvalue = val.replace(/(\d{3})(\d{3})(\d{4})/, "($1) $2-$3");
+        $(this).val(newvalue);
+    });
+});
+
+// MASK DATE
+// ==============
+
+$(document).ready(function () {
+    $('.date').blur(function () {
+        var val = $(this).val();
+        var newvalue = val.replace(/(\d{2})(\d{2})(\d{4})/, "$1/$2/$3");
+        $(this).val(newvalue);
+    });
+});
+
+// ONLY NUMBERS ALLOWED
+// ==============
+$(document).ready(function () {
+    $(".onlynumbers").bind("keypress", function (e) {
+        var keyCode = e.which ? e.which : e.keyCode
+
+        if (!(keyCode >= 48 && keyCode <= 57)) {
+            return false;
+        }
+    });
+});
